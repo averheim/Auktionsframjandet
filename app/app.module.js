8e2ca620cee367a44f5d3d8ad5f634fb33e1f315
anton <anton.neander@gmail.com>
2017-03-17 11:14:46 +0100

    add module word

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,1 @@
-angular.module("app", ["ui.bootstrap", "ngRoute", "auctions", "login", "suppliers"]);// "auction", "..."
+angular.module("app", ["ui.bootstrap", "ngRoute", "auctions", "login", "suppliers", "customer"]);// "auction", "..."
