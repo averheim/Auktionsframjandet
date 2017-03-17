52c0dca1c898383c75126ed5df83d4ca63cbb265
anton <anton.neander@gmail.com>
2017-03-17 10:51:34 +0100

    customer module service controller & template added

---
+++ app/create-new-user/customer-create.controller.js
@@ -0,0 +1,26 @@
+angular.module("customer")
+    .controller("customerCreateController", ["$scope", "$location", "$routeParams", "customerService", //login?
+        function($scope, $location, $routeParams, customerService){
+
+            $scope.customer = {};
+
+            $scope.createCustomer = function(){
+                var newCustomer = {
+                    firstName: $scope.customer.firstName,
+                    lastName: $scope.customer.lastName,
+                    email: $scope.customer.email,
+                    password: $scope.customer.password,
+                    phone: $scope.customer.phone,
+                    address: $scope.customer.address,
+                    postalcode: $scope.customer.postalcode,
+                    city: $scope.customer.city,
+                    role: Customer
+
+                };
+
+                customerService.createCustomer(newCustomer).then(function(){
+                    $location.path("/login");
+                });
+
+            };
+        }]);
---
+++ app/create-new-user/customer.service.js
@@ -0,0 +1,27 @@
+angular.module("customer")
+    .factory("customerService", ["$http", function($http){
+
+        return {
+            createCustomer: function(customer){
+                return $http.get("http://nackademiska.azurewebsites.net/api/customer", customer); //AJAX post
+            }
+        };
+        function createCustomer(customer){
+            var input = customer;
+            return $http.post('http://nackademiska.azurewebsites.net/api/account', input)
+                .then(
+                    function successCallback(response){
+                        appServiceFactory.createCustomer(true);
+                        appServiceFactory.createCustomer(response.input);
+                        return {
+                            status : true,
+                            input : response.input
+                        }
+                    },
+                    function errorCallback(response){
+                        return {
+                            status : false,
+                        }
+                    });
+        }
+    }]);
