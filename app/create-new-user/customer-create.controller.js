b639e4523688c79efe3fed73ef7a764c75875e16
Peder Waern <pederwaern@hotmail.com>
2017-03-21 10:23:38 +0100

    create customer works

--- app/create-new-user/customer-create.controller.js
+++ app/create-new-user/customer-create.controller.js
@@ -3,6 +3,8 @@ angular.module("customer")
         function($scope, $location, $routeParams, customerService){
 
             $scope.customer = {};
+            $scope.showSuccess = false;
+            $scope.showFailure = false;
 
             $scope.createCustomer = function(){
                 var newCustomer = {
@@ -13,17 +15,33 @@ angular.module("customer")
                     password: $scope.customer.password,
                     phone: $scope.customer.phone,
                     address: $scope.customer.address,
-                    postalcode: $scope.customer.postalcode,
+                    postalCode: $scope.customer.postalCode,
                     city: $scope.customer.city,
-                    role: Customer
+                    role: "Customer"
 
 
                 };
 
-                customerService.createcustomer(newCustomer).then(function(response){
+                customerService.createCustomer(newCustomer).then(function(response){
+                    $scope.showSuccess = true;
+                    $scope.showFailure = false;
 
-                    if(response.status) {
-                        $location.path("/login");}
+                    $scope.customer.firstName = "";
+                    $scope.customer.lastName = "";
+                    $scope.customer.email = "";
+                    $scope.customer.password = "";
+                    $scope.customer.phone = "";
+                    $scope.customer.address = "";
+                    $scope.customer.postalCode = "";
+                    $scope.customer.city = "";
+
+
+
+
+                }, function(error){
+                    $scope.showSuccess = false;
+                    $scope.showFailure = true;
+
                 });
 
             };
--- app/create-new-user/customer.service.js
+++ app/create-new-user/customer.service.js
@@ -3,27 +3,8 @@ angular.module("customer")
 
         return {
             createCustomer: function(customer){
-                return $http.get("http://nackademiska.azurewebsites.net/api/customer", customer); //AJAX post
+                return $http.post("http://nackademiska-api.azurewebsites.net/api/account", customer); //AJAX post
             }
         };
-        function createCustomer(customer){
 
-            var input = customer;
-
-            return $http.post('http://nackademiska.azurewebsites.net/api/account', input)
-                .then(
-                    function successCallback(response){
-                        appServiceFactory.createCustomer(true);
-                        appServiceFactory.createCustomer(response.input);
-                        return {
-                            status : true,
-                            input : response.input
-                        }
-                    },
-                    function errorCallback(response){
-                        return {
-                            status : false,
-                        }
-                    });
-        }
     }]);
